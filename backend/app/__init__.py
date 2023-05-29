d4fae6b3b53d1b23b56dc184860e3041874be2b3
John O'Donnell <johnodonnell1997@icloud.com>
2023-05-29 17:32:44 -0500

    api is functioning, time to get the routes functioning

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -30,6 +30,8 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
+app.register_blueprint(story_routes,url_prefix='/api/stories')
+app.register_blueprint(category_routes, url_prefix='/api/categories')
 db.init_app(app)
 Migrate(app, db)
 
--- backend/app/api/category_routes.py
+++ backend/app/api/category_routes.py
@@ -2,3 +2,9 @@ from flask import Blueprint, jsonify, session, request
 from app.models import Category, db
 
 category_routes = Blueprint('category', __name__)
+
+@category_routes.route("/", methods=["GET"])
+def category_home():
+    categories = Category.query.all()
+
+    return {"Categories": [item.to_dict() for item in categories]}
