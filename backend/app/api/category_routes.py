b5f3e1d54890c8ac43b0ba56530eb0fdcef41097
John O'Donnell <johnodonnell1997@icloud.com>
2023-05-29 17:40:05 -0500

    category by ID

--- backend/app/api/category_routes.py
+++ backend/app/api/category_routes.py
@@ -3,8 +3,19 @@ from app.models import Category, db
 
 category_routes = Blueprint('category', __name__)
 
+
+@category_routes.route("/<int:id>", methods=["GET"])
+def category_by_id(id):
+    category = Category.query.get(id)
+    if category:
+        return jsonify(category.to_dict()), 200
+    else:
+        return jsonify({"Message":"This category does not exist"}), 404
+
+
 @category_routes.route("/", methods=["GET"])
 def category_home():
     categories = Category.query.all()
 
     return {"Categories": [item.to_dict() for item in categories]}
+
